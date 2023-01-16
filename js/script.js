16097369f729a17d4791a4f01d5090fadce30672
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-16 21:09:40 +0100

    pass. generation via. button

---
+++ js/script.js
@@ -0,0 +1,112 @@
+const leftPassword = document.querySelector(".password-el-left");
+const rightPassword = document.querySelector(".password-el-right");
+const generatePassword = document.getElementById("generate-password");
+
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+generatePassword.addEventListener("click", generate);
+
+function generate() {
+  leftPassword.textContent = "";
+  rightPassword.textContent = "";
+
+  for (let i = 0; i < 16; i++) {
+    let rightPass = Math.floor(Math.random() * characters.length);
+    let leftPass = Math.floor(Math.random() * characters.length);
+
+    leftPassword.textContent += characters[leftPass];
+    rightPassword.textContent += characters[rightPass];
+  }
+}
